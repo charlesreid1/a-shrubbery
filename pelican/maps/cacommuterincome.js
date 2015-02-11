664b639e134e3d272b2a4957c1245c15bd115542
Charles Reid <charlesreid1@gmail.com>
2015-02-10 23:36:53 -0800

    fix map popup to include county name. other typo fixes.

--- pelican/maps/cacommuterincome.js
+++ pelican/maps/cacommuterincome.js
@@ -18,19 +18,19 @@ var m3 = L.map('map3').setView([37.7, -122.4], 6);
 // Add MapBox Tiles
 // https://www.mapbox.com/developers/api/maps/
 tl1 = L.tileLayer('http://api.tiles.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiY2hhcmxlc3JlaWQxIiwiYSI6ImpreUJGM3MifQ.w5rSM7MjHv-SnOnt3gcqHA',{
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>',
+    attribution: 'US Census Bureau',
     maxZoom: 18
 });
 tl1.addTo(m1);
 
 tl2 = L.tileLayer('http://api.tiles.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiY2hhcmxlc3JlaWQxIiwiYSI6ImpreUJGM3MifQ.w5rSM7MjHv-SnOnt3gcqHA',{
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>',
+    attribution: 'US Census Bureau',
     maxZoom: 18
 });
 tl2.addTo(m2);
 
 tl3 = L.tileLayer('http://api.tiles.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiY2hhcmxlc3JlaWQxIiwiYSI6ImpreUJGM3MifQ.w5rSM7MjHv-SnOnt3gcqHA',{
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>',
+    attribution: 'US Census Bureau',
     maxZoom: 18
 });
 tl3.addTo(m3);
@@ -39,25 +39,25 @@ tl3.addTo(m3);
 function getColorGreen(d) {
     // 6 scale greens
     var colors = ['rgb(237,248,251)','rgb(204,236,230)','rgb(153,216,201)','rgb(102,194,164)','rgb(44,162,95)','rgb(0,109,44)'];
-    return colors[Math.round(d*colors.length)];
+    return colors[Math.round((d/0.75)*colors.length)];
 };
 
 function getColorBlue(d) {
     // 6 scale blues
     var colors = ['rgb(239,243,255)','rgb(198,219,239)','rgb(158,202,225)','rgb(107,174,214)','rgb(49,130,189)','rgb(8,81,156)'];
-    return colors[Math.round(d*colors.length)];
+    return colors[Math.round((d/0.75)*colors.length)];
 };
 
 function getColorPink(d) {
     // 6 scale magenta
     var colors = ['rgb(254,235,226)','rgb(252,197,192)','rgb(250,159,181)','rgb(247,104,161)','rgb(197,27,138)','rgb(122,1,119)'];
-    return colors[Math.round(d*colors.length)];
+    return colors[Math.round((d/0.75)*colors.length)];
 };
 
 function getColorOrange(d) {
     // 6 scale OrRd
     var colors = ['rgb(254,240,217)','rgb(253,212,158)','rgb(253,187,132)','rgb(252,141,89)','rgb(227,74,51)','rgb(179,0,0)']
-    return colors[Math.round(d*colors.length)];
+    return colors[Math.round((d/0.75)*colors.length)];
 };
 
 // f = feature, l = layer
@@ -65,10 +65,10 @@ function getColorOrange(d) {
 function enhanceLayer1(f,l){
     var out = [];
     if (f.properties){
-        l.bindPopup("Percentage: "+(f.properties[key1]*100).toFixed());
+        l.bindPopup("County: "+f.properties['name']+"<br />"+"Percentage: "+(f.properties[key1]*100).toFixed());
         l.setStyle({    
             fillColor: getColorBlue(f.properties[key1]),
-            fillOpacity: 1.00,
+            fillOpacity: 0.80,
             stroke: true,
             color: '#222',
             weight: 1
@@ -79,10 +79,10 @@ function enhanceLayer1(f,l){
 function enhanceLayer2(f,l){
     var out = [];
     if (f.properties){
-        l.bindPopup("Percentage: "+(f.properties[key1]*100).toFixed());
+        l.bindPopup("County: "+f.properties['name']+"<br />"+"Percentage: "+(f.properties[key2]*100).toFixed());
         l.setStyle({    
             fillColor: getColorGreen(f.properties[key2]),
-            fillOpacity: 1.00,
+            fillOpacity: 0.80,
             stroke: true,
             color: '#222',
             weight: 1
@@ -93,10 +93,10 @@ function enhanceLayer2(f,l){
 function enhanceLayer3(f,l){
     var out = [];
     if (f.properties){
-        l.bindPopup("Percentage: "+(f.properties[key1]*100).toFixed());
+        l.bindPopup("County: "+f.properties['name']+"<br />"+"Percentage: "+(f.properties[key3]*100).toFixed());
         l.setStyle({    
             fillColor: getColorOrange(f.properties[key3]),
-            fillOpacity: 1.00,
+            fillOpacity: 0.80,
             stroke: true,
             color: '#222',
             weight: 1
