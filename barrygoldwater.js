ff74a539a8276f0e45137758db701467f6ddc7c1
Charles Reid <charlesreid1@gmail.com>
2015-02-13 20:06:15 -0800

    updating goldwater.

--- barrygoldwater.js
+++ barrygoldwater.js
@@ -5,7 +5,7 @@ var width = 800,
 // height increase = move to bottom
 var projection = d3.geo.albersUsa()
     .scale(6000)
-    .translate([width*1.8, height*0.02]);
+    .translate([width*2.0, height*0.00]);
     //.translate([width*3/4, height/2]);
 
 var path = d3.geo.path()
