9a77d2aa1a6b9968749429ac47455167e47ece12
Charles Reid <charlesreid1@gmail.com>
2015-02-15 21:56:49 -0800

    adding barry goldwater with contour overlays

--- barrygoldwater.js
+++ barrygoldwater.js
@@ -1,91 +1,240 @@
-var width = 800,
-    height = 600;
 
-// width increase = move to right
-// height increase = move to bottom
-var projection = d3.geo.albersUsa()
-    .scale(4000)
-    .translate([width*1.4, height*0.10]);
-    //.translate([width*3/4, height/2]);
+// create the map, assign to the mao div, and set it's lat, long, and zoom level (12)
+//var map = L.map('map').setView([-100.0,35.0], 12);
+var map = L.map('map').setView([32.8, -113.5], 10);
+//var map = L.map('map').setView([-113.5, 32.5], 10);
 
-var path = d3.geo.path()
-    .projection(projection)
-    .pointRadius(2);
+var attribution = '<a href="http://www.doi.gov">U.S. Department of the Interior</a> | <a href="http://www.usgs.gov">U.S. Geological Survey</a> | <a href="http://www.usgs.gov/laws/policies_notices.html">Policies</a>';
 
-var svg = d3.select("div.map").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+var tnmBasemapViewer = L.tileLayer('http://basemap.nationalmap.gov/ArcGIS/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}', {
+    maxZoom: 14,
+    attribution: attribution
+}).addTo(map);
 
+// Add baselayers and overlays to groups
+var baseLayers = {
+    "The National Map (Viewer)" : tnmBasemapViewer
+};
 
+var controlLayers = L.control.layers(baseLayers);
 
-/*
-var zoom = d3.behavior.zoom()
-    .scaleExtent([1, 2])
-    .on("zoom", zoomed);
-*/
+tnmBasemapViewer.addTo(map);
 
 
 
-// This is a hack.
-// If running locally, change prefix to /
-// Otherwise, keep a-shrubbery
-var prefix = "http://charlesreid1.github.io/a-shrubbery/";
-//var prefix = "/";
+////////////////////////////////////////////////////////////
 
-d3.json(prefix+"barrygoldwater.json", function(error, contours) {
-
-    var contour = topojson.feature(contours, contours.objects.Elev_Contour);
-
-    var onecontour = contour.features.filter(function(d) {
-        return d.properties.CONTOURELE === 1840;
-    });
-
-    // http://stackoverflow.com/questions/14492284/center-a-map-in-d3-given-a-geojson-object
-
-    var scale = 1000;
-
-    var center = d3.geo.centroid(contours);
-
-    var path2 = d3.geo.path().projection(projection);
-
-    var bounds = path2.bounds(contours);
-    var hscale  = scale*width  / (bounds[1][0] - bounds[0][0]);
-    var vscale  = scale*height / (bounds[1][1] - bounds[0][1]);
-    var scale   = (hscale < vscale) ? hscale : vscale;
-    var offset  = [width/2 - (bounds[0][0] + bounds[1][0])/2,
-                   height/2 - (bounds[0][1] + bounds[1][1])/2];
-
-    //// new projection
-    //projection2 = d3.geo.mercator().center(center)
-    //        .scale(scale).translate(offset);
-    //path2 = path2.projection(projection);
-
-
-    // add a rectangle to see the bound of the svg
-    svg.append("rect").attr('width', width).attr('height', height)
-      .style('stroke', 'black').style('fill', 'none');
-
-
-    //console.log("scale = "+scale);
-    //console.log("hscale = "+hscale);
-    //console.log("vscale = "+vscale);
-
-
-        //.data(contour.features)
-    svg.selectAll("path")
-        .data(onecontour)
-        .enter().append("path")
-        .attr("d", path)
-        .attr("class", function(d) { return "contour contour" + d.properties.CONTOURELE; })
-        .style("stroke-width", "1")
-        .style("stroke", "black")
-        .style("fill", "red")
-
+L.TopoJSON = L.GeoJSON.extend({  
+  addData: function(jsonData) {    
+    if (jsonData.type === "Topology") {
+      for (key in jsonData.objects) {
+        geojson = topojson.feature(jsonData, jsonData.objects[key]);
+        L.GeoJSON.prototype.addData.call(this, geojson);
+      }
+    }    
+    else {
+      L.GeoJSON.prototype.addData.call(this, jsonData);
+    }
+  }  
 });
 
-/*
-function zoomed() {
-    map.attr("transform", "translate(" + d3.event.translate + ") scale(" + d3.event.scale + ")");
-    contour.style("stroke-width", 0.5 / d3.event.scale);
+function enhanceLayer(f,l){
+    if (f.properties){
+        l.setStyle({    
+            fillColor: '#000',
+            fillOpacity: 0.50,
+            stroke: true,
+            color: '#000',
+            weight: 1
+        });
+    }
 }
+
+
+
+
+
+//var prefix = "http://charlesreid1.github.io/a-shrubbery/";
+var prefix = "/";
+var url = prefix+"barrygoldwater_red.json"
+
+
+
+
+var colorScale = chroma  
+  .scale(['#D5FFE3', '#007131'])
+  .domain([0,1]);
+
+
+
+
+var topoLayer = new L.TopoJSON();
+
+function addTopoData(topoData){  
+    topoLayer.addData(topoData);
+    topoLayer.addTo(map);
+
+    // iterate over each layer
+    topoLayer.eachLayer(handleLayer);  
+}
+
+$.getJSON(url)
+  .done(addTopoData);
+
+
+
+function handleLayer(layer){  
+
+  var randomValue = Math.random(),
+    fillColor = colorScale(randomValue).hex();
+
+
+  layer.setStyle({
+    fillColor : fillColor,
+    fillOpacity: 1,
+    color:'#555',
+    weight:3,
+    opacity:0.5
+  });
+
+
+  layer.on({
+    mouseover: enterLayer,
+    mouseout: leaveLayer
+  });
+}
+
+
+var $tooltip = $('.elevation');
+
+function enterLayer(){  
+  var elevation = this.feature.properties.CONTOURELE;
+  //$tooltip.text("Selected contour: "+elevation+" m").show();
+  $tooltip.text("Selected contour: "+elevation+" m").show();
+
+  this.bringToFront();
+  this.setStyle({
+    weight:3,
+    opacity: 1
+  });
+}
+
+function leaveLayer(){  
+  //$tooltip.hide();
+  $tooltip.text("Selected contour:").show();
+
+  this.bringToBack();
+  this.setStyle({
+    weight:3,
+    opacity:.8
+  });
+}
+
+//var geoj = new L.geoJson.ajax(url,{onEachFeature: enhanceLayer});
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////
+//// old stuff
+
+
+
+//function enhanceLayer(f,l){
+//    if (f.properties){
+//        if (f.properties['CONTOURELE']===1840){
+//            l.setStyle({    
+//                fillColor: '#000',
+//                fillOpacity: 0.50,
+//                stroke: true,
+//                color: '#222',
+//                weight: 1
+//            });
+//        }
+//    }
+//}
+//
+
+
+
+
+//var prefix = "http://charlesreid1.github.io/a-shrubbery/";
+//var prefix = "/";
+//var url = prefix+"barrygoldwater.geojson"
+//var geoj = new L.geoJson.ajax(url,{onEachFeature: enhanceLayer});
+/*function(f,l) {
+            if (f.properties){
+                if (f.properties['CONTOURELE']===1840){
+                    l.setStyle({    
+                        fillColor: '#000',
+                        fillOpacity: 0.50,
+                        stroke: true,
+                        color: '#222',
+                        weight: 1
+                    });
+                }
+            }
+        }
+    });
 */
+//geoj.addTo(map);
+
+
+///////////////////////////////////////////////////
+
+/*
+
+var owsrootUrl = 'http://104.236.163.66:8080/geoserver/ows';
+
+var defaultParameters = {
+    service : 'WFS',
+    version : '1.0',
+    request : 'GetFeature',
+    typeName : 'US_ShalePlays_EIA_May2011',
+    maxFeatures : '200',
+    outputFormat : 'text/javascript',
+    format_options : 'callback:getJson',
+    SrsName : 'EPSG:4269'
+};
+
+var parameters = L.Util.extend(defaultParameters);
+var URL = owsrootUrl + L.Util.getParamString(parameters);
+
+//ajax to get map features
+$.ajax({
+    type: "POST",
+    url: URL,
+    dataType: 'jsonp',
+    jsonpCallback : 'getJson',
+
+    //upon success extraction of data
+    success: function (data) {
+
+        console.log(data);
+
+        // the data.geometry field contains coordinate/location data.
+        //
+        // now use this page on Leaflet+GeoJSON:
+        // http://leafletjs.com/examples/geojson.html
+
+    	//create a new geojson layer
+    	var geojson = new L.geoJson(data, {
+
+    		    // apply a style 
+                style: {"color":"#ff7800","weight":2},
+                //
+    		    // and bind a popup showing the street name for each feature extracted.
+    		    //onEachFeature: function(feature, layer){
+    		    //	layer.bindPopup("street: " + feature.properties.name);
+                //    //console.log(feature.properties);
+    		    //}
+    	}).addTo(map);
+
+        //var marker = L.marker([40.7543, -73.9858]).addTo(map);
+    }
+});
+*/
+
