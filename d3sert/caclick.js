bd0966db06ca4cd7fa8b28db9a8de77e002f4ae6
Charles Reid <charlesreid1@gmail.com>
2015-02-18 14:58:34 -0800

    updating ca click example. working legend. d3 graphs are never finished, but this one is pretty damn close.

--- d3sert/caclick.js
+++ d3sert/caclick.js
@@ -15,46 +15,6 @@ function getColorBlue(d) {
     return colors[Math.round((d/0.75)*colors.length)];
 };
 
-
-
-/*
-{
-"A_Below100PovLn_BikeEtc": 122.0,
-"A_Below100PovLn_BikeEtc_CountyPct": 0.02109631679059312,
-"A_Below100PovLn_DroveAlone": 4175.0,
-"A_Below100PovLn_DroveAlone_CountyPct": 0.7219436278748055,
-"A_Below100PovLn_DroveCarpool": 651.0,
-"A_Below100PovLn_DroveCarpool_CountyPct": 0.11257132975964032,
-"A_Below100PovLn_PublicTrans": 69.0,
-"A_Below100PovLn_PublicTrans_CountyPct": 0.011931523430745288,
-"A_Below100PovLn_Walked": 216.0,
-"A_Below100PovLn_Walked_CountyPct": 0.03735085595711568,
-"B_Btwn100_149PovLn_BikeEtc": 102.0,
-"B_Btwn100_149PovLn_BikeEtc_CountyPct": 0.01766845660834921,
-"B_Btwn100_149PovLn_DroveAlone": 4579.0,
-"B_Btwn100_149PovLn_DroveAlone_CountyPct": 0.793175125584618,
-"B_Btwn100_149PovLn_DroveCarpool": 593.0,
-"B_Btwn100_149PovLn_DroveCarpool_CountyPct": 0.10271955655638317,
-"B_Btwn100_149PovLn_PublicTrans": 52.0,
-"B_Btwn100_149PovLn_PublicTrans_CountyPct": 0.009007448467001558,
-"B_Btwn100_149PovLn_Walked": 205.0,
-"B_Btwn100_149PovLn_Walked_CountyPct": 0.035510133379525376,
-"C_Above150PovLn_BikeEtc": 1098.0,
-"C_Above150PovLn_BikeEtc_CountyPct": 0.020321667191056984,
-"C_Above150PovLn_DroveAlone": 44567.0,
-"C_Above150PovLn_DroveAlone_CountyPct": 0.8248412948122374,
-"C_Above150PovLn_DroveCarpool": 4246.0,
-"C_Above150PovLn_DroveCarpool_CountyPct": 0.07858451629620034,
-"C_Above150PovLn_PublicTrans": 371.0,
-"C_Above150PovLn_PublicTrans_CountyPct": 0.006866428531768799,
-"C_Above150PovLn_Walked": 754.0,
-"C_Above150PovLn_Walked_CountyPct": 0.013954951786937128,
-"geoid": "05000US06089",
-"name": "Shasta County, CA"
-}
-*/
-
-
 key1 = 'A_Below100PovLn_PublicTrans_CountyPct'
 
 function enhanceLayer1(f,l){
@@ -169,26 +129,23 @@ function layerMouseclick() {
     this.feature.properties['A_Below100PovLn_BikeEtc_CountyPct'];
 
     // Assemble our pie chart data
-    key1 = 'A_Below100PovLn_BikeEtc_CountyPct';
-    key2 = 'A_Below100PovLn_DroveAlone_CountyPct';
-    key3 = 'A_Below100PovLn_DroveCarpool_CountyPct';
-    key4 = 'A_Below100PovLn_PublicTrans_CountyPct';
-    key5 = 'A_Below100PovLn_Walked_CountyPct';
+    key1 = 'A_Below100PovLn_Walked_CountyPct';
+    key2 = 'A_Below100PovLn_BikeEtc_CountyPct';
+    key3 = 'A_Below100PovLn_PublicTrans_CountyPct';
+    key4 = 'A_Below100PovLn_DroveCarpool_CountyPct';
+    key5 = 'A_Below100PovLn_DroveAlone_CountyPct';
 
-    lab1 = 'Biked'
-    lab2 = 'Drove Alone'
-    lab3 = 'Drove Carpool'
-    lab4 = 'Public Transit'
-    lab5 = 'Walked'
+    lab1 = 'Walked'
+    lab2 = 'Biked'
+    lab3 = 'Public Transit'
+    lab4 = 'Drove Carpool'
+    lab5 = 'Drove Alone'
 
     pie_data = [{'cat' : lab1, 'key' : key1, 'dat' : this.feature.properties[key1]},
                 {'cat' : lab2, 'key' : key2, 'dat' : this.feature.properties[key2]},  
                 {'cat' : lab3, 'key' : key3, 'dat' : this.feature.properties[key3]},  
                 {'cat' : lab4, 'key' : key4, 'dat' : this.feature.properties[key4]},  
                 {'cat' : lab5, 'key' : key5, 'dat' : this.feature.properties[key5]}]  
-
-    //console.log("Data for "+this.feature.properties['name']);
-    //console.log(pie_data);
 
 
 
@@ -206,26 +163,18 @@ function layerMouseclick() {
     //make our initial drawing...
     //
 
-    console.log("Path, just before our first transition:");
-    path.each(function(d,i){ console.log(d);});
-    //
+    //console.log("Path, just before our first transition:");
+    //path.each(function(d,i){ console.log(d);});
+
     // path[0] = {'startAngle' : 0
     //            'endAngle' : 1.25}
     //
     // path[1] = {'startAngle' : 1.25,
     //            'endAngle' : 2.51
-    //            
 
 
     var data0 = path.data(); 
     var data1 = pie(pie_data);
-
-    //console.log("Nclicks: "+Nclicks);
-    //console.log("Data0 to Data1 transition:");
-    //console.log("Data0:");
-    //console.log(data0);
-    //console.log("Data1:");
-    //console.log(data1);
 
     path = path.data(data1, key);
 
@@ -238,10 +187,10 @@ function layerMouseclick() {
     //
     path.enter().append("path")
         .each(function(d, i) { 
-            console.log("Data0:");
-            console.log(data0);
-            console.log("Data1:");
-            console.log(data1);
+            //console.log("Data0:");
+            //console.log(data0);
+            //console.log("Data1:");
+            //console.log(data1);
             this._current = findNeighborArc(i, data0, data1, key) || d; 
         })
         .attr("fill", function(d) { 
@@ -259,211 +208,29 @@ function layerMouseclick() {
     path.transition()
         .duration(750)
         .attrTween("d", arcTween);
+
     ////////////////////////////////////////////
 
 
     ///// Arc Labels ///// 
     // Calculate position 
-    path.append("text")
-        .attr("transform", function(d) { 
+    //txts.remove();
+    //txts = path.append("text")
+    //
+    // YOU ARE HERE
+    //
+    // This is correctly computing new arcs from the centroid.
+    // However, updating the text label location isn't working.
+    //
+    txts.attr("transform", function(d) { 
+            //console.log(d.data.cat);
+            //console.log(arc.centroid(d));
             return "translate(" + arc.centroid(d) + ")"; })
         .attr("dy", ".35em")
         .style("text-anchor", "middle")
         .text(function(d) { 
             return d.data.cat; 
         });
-
-
-
-
-    // trying a third time to get 
-    // some damn text labels 
-    // on this damn pie chart.
-    //
-    // http://jsfiddle.net/nrabinowitz/GQDUS/
-
-    /*
-    var labelr = radius + 30;
-    var mytxt = path.enter().append("path")
-            .each(function(d, i) { this._current = findNeighborArc(i, data0, data1, key) || d; })
-            .attr("fill", function(d) { 
-                return color(d.data.cat); })
-            .append("text")
-    mytxt.attr("transform", function(d) {
-            var c = arc.centroid(d),
-                x = c[0],
-                y = c[1],
-                // pythagorean theorem for hypotenuse
-                h = Math.sqrt(x*x + y*y);
-            return "translate(" + (x/h * labelr) +  ',' +
-               (y/h * labelr) +  ")"; 
-        })
-        .attr("dy", ".35em")
-        .attr("text-anchor", function(d) {
-            // are we past the center?
-            return (d.endAngle + d.startAngle)/2 > Math.PI ?
-                "end" : "start";
-        })
-        .text(function(d,i) { 
-            return d.data.cat; });
-    */
-
-
-
-    /*
-
-    // Now add text labels
-    //
-    // don't ask...
-    //
-    // http://jsfiddle.net/thudfactor/HdwTH/
-    // 
-    pied_arc = d3.svg.arc()
-        .innerRadius(radius - 70)
-        .outerRadius(radius - 10);
-
-    enteringLabels = svg.select(".labels").data(categories).enter();
-    labelGroups = enteringLabels.append("g").attr("class", "label");
-    labelGroups.append("circle").attr({
-        x: 0,
-        y: 0,
-        r: 2,
-        fill: "#000",
-        transform: function (d, i) {
-            centroid = pied_arc.centroid(d);
-            return "translate(" + pied_arc.centroid(d) + ")";
-        },
-            'class': "label-circle"
-    });
-
-    // "When am I ever going to use this?" I said in 
-    // 10th grade trig.
-    cDim = {'labelRadius':175};
-    textLines = labelGroups.append("line").attr({
-        x1: function (d, i) {
-            return pied_arc.centroid(d)[0];
-        },
-        y1: function (d, i) {
-            return pied_arc.centroid(d)[1];
-        },
-        x2: function (d, i) {
-            centroid = pied_arc.centroid(d);
-            midAngle = Math.atan2(centroid[1], centroid[0]);
-            x = Math.cos(midAngle) * cDim.labelRadius;
-            return x;
-        },
-        y2: function (d, i) {
-            centroid = pied_arc.centroid(d);
-            midAngle = Math.atan2(centroid[1], centroid[0]);
-            y = Math.sin(midAngle) * cDim.labelRadius;
-            return y;
-        },
-            'class': "label-line"
-    });
-
-
-
-    console.log(labelGroups);
-    textLabels = labelGroups.append("text").attr({
-        x: function (d, i) {
-            centroid = pied_arc.centroid(d);
-            midAngle = Math.atan2(centroid[1], centroid[0]);
-            x = Math.cos(midAngle) * cDim.labelRadius;
-            sign = (x > 0) ? 1 : -1
-            labelX = x + (5 * sign)
-            return labelX;
-        },
-        y: function (d, i) {
-            centroid = pied_arc.centroid(d);
-            midAngle = Math.atan2(centroid[1], centroid[0]);
-            y = Math.sin(midAngle) * cDim.labelRadius;
-            console.log(d);
-            return y;
-        },
-            'text-anchor': function (d, i) {
-            centroid = pied_arc.centroid(d);
-            midAngle = Math.atan2(centroid[1], centroid[0]);
-            x = Math.cos(midAngle) * cDim.labelRadius;
-            return (x > 0) ? "start" : "end";
-        },
-            'class': 'label-text'
-    })
-    .text(function (d) {
-        return d;
-    });
-
-    */
-
-
-    /*
-    // now add text labels
-    // 
-    // via 
-    // http://bl.ocks.org/dbuezas/9306799
-    //
-    var text = svg.select(".labels").selectAll("text")
-        .data(data1, key);
-
-    text.enter()
-        .append("text")
-        .attr("dy", ".35em")
-        .text(function(d) {
-            return d.data.label;
-        });
-    
-    function midAngle(d){
-        return d.startAngle + (d.endAngle - d.startAngle)/2;
-    }
-
-    text.transition().duration(1000)
-        .attrTween("transform", function(d) {
-            this._current = this._current || d;
-            var interpolate = d3.interpolate(this._current, d);
-            this._current = interpolate(0);
-            return function(t) {
-                var d2 = interpolate(t);
-                var pos = outerArc.centroid(d2);
-                pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
-                return "translate("+ pos +")";
-            };
-        })
-        .styleTween("text-anchor", function(d){
-            this._current = this._current || d;
-            var interpolate = d3.interpolate(this._current, d);
-            this._current = interpolate(0);
-            return function(t) {
-                var d2 = interpolate(t);
-                return midAngle(d2) < Math.PI ? "start":"end";
-            };
-        });
-
-    text.exit()
-        .remove();
-
-
-    var polyline = svg.select(".lines").selectAll("polyline")
-        .data(data1, key);
-    
-    polyline.enter()
-        .append("polyline");
-
-    polyline.transition().duration(1000)
-        .attrTween("points", function(d){
-            this._current = this._current || d;
-            var interpolate = d3.interpolate(this._current, d);
-            this._current = interpolate(0);
-            return function(t) {
-                var d2 = interpolate(t);
-                var pos = outerArc.centroid(d2);
-                pos[0] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
-                return [arc.centroid(d2), outerArc.centroid(d2), pos];
-            };          
-        });
-    
-    polyline.exit()
-        .remove();
-    
-        */
 
 }
 
@@ -501,8 +268,6 @@ function findFollowing(i, data0, data1, key) {
 }
 
 function arcTween(d) {
-    //console.log("Tween: this._current =");
-    //console.log(this._current);
     var i = d3.interpolate(this._current, d);
     this._current = i(0);
     return function(t) { return arc(i(t)); };
@@ -535,14 +300,14 @@ var geoj1 = new L.geoJson.ajax(
 var categories = ["Walked", "Biked", "Public Transit", "Drove Carpool", "Drove Alone"];
 
 
-var width = 300,
-    height = 300,
-    padding = 30,
+var width = 350,
+    height = 500,
+    padding = 60,
     radius = Math.min(width-padding, height-padding) / 2;
 
 var color = d3.scale.ordinal()
     .domain(categories)
-    .range(["#018571", "#80cdc1", "#f5f5f5", "#dfc27d", "#a6611a"]);
+    .range(["#018571", "#80cdc1", "#ddd", "#dfc27d", "#a6611a"]);
 
 var arc = d3.svg.arc()
     .outerRadius(radius - 10)
@@ -552,17 +317,17 @@ var outerArc = d3.svg.arc()
     .innerRadius(radius * 0.9)
     .outerRadius(radius * 0.9);
 
-lab1 = 'Biked';
-lab2 = 'Drove Alone';
-lab3 = 'Drove Carpool';
-lab4 = 'Public Transit';
-lab5 = 'Walked';
+lab1 = 'Walked'
+lab2 = 'Biked'
+lab3 = 'Public Transit'
+lab4 = 'Drove Carpool'
+lab5 = 'Drove Alone'
+
 init_data = [{'cat' : lab1, 'dat' : 1.0},
              {'cat' : lab2, 'dat' : 1.0},  
              {'cat' : lab3, 'dat' : 1.0},  
              {'cat' : lab4, 'dat' : 1.0},  
              {'cat' : lab5, 'dat' : 1.0}];
-
 
 var pie = d3.layout.pie()
     .sort(null)
@@ -577,7 +342,6 @@ var svg = d3.select("div.graph").append("svg")
 svg.append("g").attr("class","labels");
 svg.append("g").attr("class","lines");
 
-
 var g = svg.selectAll(".arc")
     .data(pie(init_data))
     .enter().append("g")
@@ -585,9 +349,14 @@ var g = svg.selectAll(".arc")
 
 g.append("path")
     .attr("d", arc)
+    .attr("data-legend", function(d){
+        return d.data.cat; })
+    .attr("data-legend-pos",function(d,i) {
+        return categories[i]; })
     .style("fill", function(d) { 
         return color(d.data.cat); });
 
+/*
 g.append("text")
     .attr("transform", function(d) { 
         return "translate(" + arc.centroid(d) + ")"; })
@@ -596,6 +365,15 @@ g.append("text")
     .text(function(d) { 
         return d.data.cat; 
     });
+*/
+
+
+legend = svg.append("g")
+    .attr("class", "legend")
+    .attr("transform", "translate(80,-150)")
+    .style("font-size", "12px")
+    .call(d3.legend)
+
 
 var path = svg.selectAll("path");
 
@@ -618,19 +396,7 @@ data_init1 = pie(init_data);
 path = path.data(data_init1, key);
 
 
-// animate transitions...
-/*
-path.enter().append("path")
-    .each(function(d, i) { 
-        this._current = findNeighborArc(i, data_init0, data_init1, key) || d; 
-    })
-    .attr("fill", function(d) { 
-        return color(d.data.cat); })
-
-path.each(function(d){console.log(this._current);});
-
-(5 x undefined)
-*/
+// Animate transitions...
 path.each(function(d, i) { 
         this._current = findNeighborArc(i, data_init0, data_init1, key) || d; 
     })
@@ -652,7 +418,7 @@ path.transition()
     .duration(750)
     .attrTween("d", arcTween);
 
-path.append("text")
+txts = path.append("text")
     .attr("transform", function(d) { 
         return "translate(" + arc.centroid(d) + ")"; })
     .attr("dy", ".35em")
@@ -757,4 +523,47 @@ d3.csv("data.csv", function(error, data) {
 
 });
 */
+
+
+
+
+
+
+/*
+{
+"A_Below100PovLn_BikeEtc": 122.0,
+"A_Below100PovLn_BikeEtc_CountyPct": 0.02109631679059312,
+"A_Below100PovLn_DroveAlone": 4175.0,
+"A_Below100PovLn_DroveAlone_CountyPct": 0.7219436278748055,
+"A_Below100PovLn_DroveCarpool": 651.0,
+"A_Below100PovLn_DroveCarpool_CountyPct": 0.11257132975964032,
+"A_Below100PovLn_PublicTrans": 69.0,
+"A_Below100PovLn_PublicTrans_CountyPct": 0.011931523430745288,
+"A_Below100PovLn_Walked": 216.0,
+"A_Below100PovLn_Walked_CountyPct": 0.03735085595711568,
+"B_Btwn100_149PovLn_BikeEtc": 102.0,
+"B_Btwn100_149PovLn_BikeEtc_CountyPct": 0.01766845660834921,
+"B_Btwn100_149PovLn_DroveAlone": 4579.0,
+"B_Btwn100_149PovLn_DroveAlone_CountyPct": 0.793175125584618,
+"B_Btwn100_149PovLn_DroveCarpool": 593.0,
+"B_Btwn100_149PovLn_DroveCarpool_CountyPct": 0.10271955655638317,
+"B_Btwn100_149PovLn_PublicTrans": 52.0,
+"B_Btwn100_149PovLn_PublicTrans_CountyPct": 0.009007448467001558,
+"B_Btwn100_149PovLn_Walked": 205.0,
+"B_Btwn100_149PovLn_Walked_CountyPct": 0.035510133379525376,
+"C_Above150PovLn_BikeEtc": 1098.0,
+"C_Above150PovLn_BikeEtc_CountyPct": 0.020321667191056984,
+"C_Above150PovLn_DroveAlone": 44567.0,
+"C_Above150PovLn_DroveAlone_CountyPct": 0.8248412948122374,
+"C_Above150PovLn_DroveCarpool": 4246.0,
+"C_Above150PovLn_DroveCarpool_CountyPct": 0.07858451629620034,
+"C_Above150PovLn_PublicTrans": 371.0,
+"C_Above150PovLn_PublicTrans_CountyPct": 0.006866428531768799,
+"C_Above150PovLn_Walked": 754.0,
+"C_Above150PovLn_Walked_CountyPct": 0.013954951786937128,
+"geoid": "05000US06089",
+"name": "Shasta County, CA"
+}
+*/
+
 
