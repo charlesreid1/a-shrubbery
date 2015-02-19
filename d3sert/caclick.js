b5dc0b143f93ddc80a07a99cc31713ba8cec2818
Charles Reid <charlesreid1@gmail.com>
2015-02-18 21:23:35 -0800

    IT WORKS!!!

--- d3sert/caclick.js
+++ d3sert/caclick.js
@@ -149,6 +149,9 @@ function layerMouseclick() {
 
 
 
+
+
+
     // This basically uses data0 and data1 to 
     // update new locations for pie slices.
     // it then draws the new ones. 
@@ -212,10 +215,15 @@ function layerMouseclick() {
     ////////////////////////////////////////////
 
 
-    ///// Arc Labels ///// 
+    // Labels
+
     // Calculate position 
     //txts.remove();
     //txts = path.append("text")
+
+    /*
+    // 02/18/2015
+    // early evening
     //
     // YOU ARE HERE
     //
@@ -231,6 +239,97 @@ function layerMouseclick() {
         .text(function(d) { 
             return d.data.cat; 
         });
+    */
+
+    // --------------------------------------------------------
+    // Start labels code
+    // 
+    // from http://bl.ocks.org/dbuezas/9306799
+    // adding labels with lines
+    //
+    // note that there isn't a PIE, 
+    // per se, just a bunch of things
+    // using a pie() function to split 
+    // information apart and use it for 
+    // drawing shapes.
+    var text = svg.select(".labels").selectAll("text")
+        .data(data1);
+        //.data(pie(pie_data));
+        //.data(pie(init_data), key);
+    
+    text.enter()
+        .append("text")
+        .attr("dy", ".35em")
+        .style("font-size", "11px")
+        .attr("transform", function(d) { 
+            return "translate(" + arc.centroid(d) + ")"; })
+        .style("text-anchor", "middle")
+        .text(function(d) {
+            return d.data.cat;
+        });
+    
+    //function midAngle(d){
+    //    return d.startAngle + (d.endAngle - d.startAngle)/2;
+    //}
+    
+    
+    text.transition().duration(1000)
+        .attrTween("transform", function(d) {
+            this._current = this._current || d;
+            var interpolate = d3.interpolate(this._current, d);
+            this._current = interpolate(0);
+            return function(t) {
+                var d2 = interpolate(t);
+                var pos = outerArc.centroid(d2);
+                pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
+                //pos[1] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
+                return "translate("+ pos +")";
+            };
+        })
+        .styleTween("text-anchor", function(d){
+            this._current = this._current || d;
+            var interpolate = d3.interpolate(this._current, d);
+            this._current = interpolate(0);
+            return function(t) {
+                var d2 = interpolate(t);
+                return midAngle(d2) < Math.PI ? "start":"end";
+            };
+        });
+    
+    text.exit()
+        .remove();
+    
+    
+    //polylines
+    
+    var polyline = svg.select(".lines").selectAll("polyline")
+        .data(data1);
+    
+    polyline.enter()
+        .append("polyline");
+    
+    polyline.transition().duration(1000)
+        .attrTween("points", function(d){
+            this._current = this._current || d;
+            var interpolate = d3.interpolate(this._current, d);
+            this._current = interpolate(0);
+            return function(t) {
+                var d2 = interpolate(t);
+                var pos = outerArc.centroid(d2);
+                pos[0] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
+                //pos[1] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
+                return [arc.centroid(d2), outerArc.centroid(d2), pos];
+            };          
+        });
+    
+    polyline.exit()
+        .remove();
+    
+    
+    // End labels code
+    // ----------------------------------------------------------
+
+
 
 }
 
@@ -277,9 +376,16 @@ function arcTween(d) {
 
 
 
+
+
+
+
+
+
+
+
+
 /////////////////////////////////////////////////////////////////
-
-
 // add geojson to map
 
 
@@ -289,6 +395,14 @@ var geoj1 = new L.geoJson.ajax(
                     prefix+"cacommuterincome.geojson",
                     {onEachFeature : enhanceLayer1}
                 ).addTo(map);
+
+
+// end add geojson
+/////////////////////////////////////////////////////////////////
+
+
+
+
 
 
 
@@ -382,6 +496,7 @@ txts = g.append("text")
 */
 
 
+    
 // --------------------------------------------------------
 // Start labels code
 // 
@@ -440,7 +555,7 @@ text.exit()
     .remove();
 
 
-/* ------- SLICE TO TEXT POLYLINES -------*/
+//polylines
 
 var polyline = svg.select(".lines").selectAll("polyline")
     .data(pie(init_data), key);
@@ -468,7 +583,6 @@ polyline.exit()
 
 // End labels code
 // ----------------------------------------------------------
-
 
 
 // add a legend 
@@ -524,143 +638,12 @@ path.transition()
     .duration(750)
     .attrTween("d", arcTween);
 
+// end d3 geom 
+/////////////////////////////////////////////////////////////////
 
 
 
 
 
-
-
-
-/*
-
-
-
-// Make sure we can make a very simple pie chart
-// from an array variable
-//
-// Here's what CSV/JSON data looks like when 
-// imported, and so how we have to organize
-// our data by hand.
-var mydat = [{'cat' : 'A', 'dat' : 0.40 },
-             {'cat' : 'B', 'dat' : 0.80 },
-             {'cat' : 'C', 'dat' : 0.15 },
-             {'cat' : 'D', 'dat' : 0.16 },
-             {'cat' : 'E', 'dat' : 0.23 }];
-
-mydat.forEach(function(d) {
-    //console.log(d.cat);
-    //console.log(d.dat);
-    d.dat = +d.dat;
-}); 
-
-
-
-// If we're using a Javascript variable,
-// we don't need the callback function
-// provided by d3.csv()
-//
-// We can just set to work on the 
-// drawing directly:
-//
-
-var g = svg.selectAll(".arc")
-    .data(pie(mydat))
-    .enter().append("g")
-    .attr("class","arc");
-
-g.append("path")
-    .attr("d", arc)
-    .style("fill", function(d) { return color(d.data.dat); });
-
-g.append("text")
-    .attr("transform", function(d) { 
-        //console.log(d.dat); 
-        //console.log(arc);
-        return "translate(" + arc.centroid(d) + ")"; })
-    .attr("dy", ".35em")
-    .style("text-anchor", "middle")
-    .text(function(d) { 
-        console.log(d.data.cat); 
-        return d.data.cat; 
-    });
-
-
-
-*/
-
-
-
-
-
-/*
-// Make a pie chart from CSV data
-// (useful, but not what we want)
-// 
-d3.csv("data.csv", function(error, data) {
-
-  data.forEach(function(d) {
-    d.population = +d.population;
-  });
-
-  var g = svg.selectAll(".arc")
-      .data(pie(data))
-    .enter().append("g")
-      .attr("class", "arc");
-
-  g.append("path")
-      .attr("d", arc)
-      .style("fill", function(d) { return color(d.data.age); });
-
-  g.append("text")
-      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-      .attr("dy", ".35em")
-      .style("text-anchor", "middle")
-      .text(function(d) { return d.data.age; });
-
-});
-*/
-
-
-
-
-
-
-/*
-{
-"A_Below100PovLn_BikeEtc": 122.0,
-"A_Below100PovLn_BikeEtc_CountyPct": 0.02109631679059312,
-"A_Below100PovLn_DroveAlone": 4175.0,
-"A_Below100PovLn_DroveAlone_CountyPct": 0.7219436278748055,
-"A_Below100PovLn_DroveCarpool": 651.0,
-"A_Below100PovLn_DroveCarpool_CountyPct": 0.11257132975964032,
-"A_Below100PovLn_PublicTrans": 69.0,
-"A_Below100PovLn_PublicTrans_CountyPct": 0.011931523430745288,
-"A_Below100PovLn_Walked": 216.0,
-"A_Below100PovLn_Walked_CountyPct": 0.03735085595711568,
-"B_Btwn100_149PovLn_BikeEtc": 102.0,
-"B_Btwn100_149PovLn_BikeEtc_CountyPct": 0.01766845660834921,
-"B_Btwn100_149PovLn_DroveAlone": 4579.0,
-"B_Btwn100_149PovLn_DroveAlone_CountyPct": 0.793175125584618,
-"B_Btwn100_149PovLn_DroveCarpool": 593.0,
-"B_Btwn100_149PovLn_DroveCarpool_CountyPct": 0.10271955655638317,
-"B_Btwn100_149PovLn_PublicTrans": 52.0,
-"B_Btwn100_149PovLn_PublicTrans_CountyPct": 0.009007448467001558,
-"B_Btwn100_149PovLn_Walked": 205.0,
-"B_Btwn100_149PovLn_Walked_CountyPct": 0.035510133379525376,
-"C_Above150PovLn_BikeEtc": 1098.0,
-"C_Above150PovLn_BikeEtc_CountyPct": 0.020321667191056984,
-"C_Above150PovLn_DroveAlone": 44567.0,
-"C_Above150PovLn_DroveAlone_CountyPct": 0.8248412948122374,
-"C_Above150PovLn_DroveCarpool": 4246.0,
-"C_Above150PovLn_DroveCarpool_CountyPct": 0.07858451629620034,
-"C_Above150PovLn_PublicTrans": 371.0,
-"C_Above150PovLn_PublicTrans_CountyPct": 0.006866428531768799,
-"C_Above150PovLn_Walked": 754.0,
-"C_Above150PovLn_Walked_CountyPct": 0.013954951786937128,
-"geoid": "05000US06089",
-"name": "Shasta County, CA"
-}
-*/
 
 
