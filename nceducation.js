2ec1516fb17c90a728f81bea1dcace4d2dc7bd5e
Charles Reid <charlesreid1@gmail.com>
2015-02-21 20:09:21 -0800

    updating NC map to implement county map with census map inset.

--- nceducation.js
+++ nceducation.js
@@ -2,8 +2,8 @@
 ////////////////////////////////////
 // Take care of county map first:
 //
-
-var map_county = L.map('education_county').setView([35.8, -78.6], 7);
+var zoomOrig = 6;
+var map_county = L.map('education_county').setView([35.8, -78.6], zoomOrig);
 
 var basemapViewer = L.tileLayer('http://api.tiles.mapbox.com/v4/mapbox.run-bike-hike/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiY2hhcmxlc3JlaWQxIiwiYSI6ImpreUJGM3MifQ.w5rSM7MjHv-SnOnt3gcqHA',{ 
     maxZoom: 14,
@@ -18,6 +18,7 @@ var basemapViewer = L.tileLayer('http://api.tiles.mapbox.com/v4/mapbox.run-bike-
 //
 //basemapViewer.addTo(map_county);
 
+// counties in north carolina
 rooturl = "http://api.censusreporter.org/1.0/geo/show/tiger2013?geo_ids=050|04000US37";
 
 // initialize empty GeoJson
@@ -108,7 +109,8 @@ function doClick() {
 
 
 
-    red = '#00ff00'
+    // http://en.wikipedia.org/wiki/Carolina_blue
+    red = '#56A0D3';
 
     these_layer_ids = Object.keys(this._layers);
 
@@ -142,35 +144,6 @@ function doClick() {
             }
         });
     });
-
-
-
-
-
-
-
-
-    /*
-    geoj.eachLayer(function(layer) {
-        console.log(layer);
-        if(layer['_options']) {
-            if( layer['_options']['fillColor'] ) {
-                if(layer['_options']['fillColor']===red) {
-                    //console.log("Returning active fill color back to original fill color.");
-                    //console.log("Before: "+layer.options['fillColor']);
-                    layer.setStyle(
-                        {
-                            'fillColor'   : layer['_options']['originalFillColor'],
-                            'fillOpacity' : myFillOpacity
-                        }
-                    )
-                    //console.log("After: "+layer.options['fillColor']);
-                    //console.log("Original: "+layer.options['originalFillColor']);
-                }
-            }
-        }
-    });
-    */
 
 
 
@@ -224,6 +197,98 @@ function doClick() {
         }
     });
 
+
+
+    // Now add census tracts for this county to the census tract map.
+    //
+    // Do this by grabbing the geo_id for the county,
+    // and form that into a Census Reporter API URL.
+
+    var geo_id = this.feature.properties.geoid;
+
+    census_url = "http://api.censusreporter.org/1.0/geo/show/tiger2013?geo_ids=140|"+geo_id;
+
+
+
+
+    // initialize empty GeoJson
+    var census_geoj = L.geoJson(false, {
+            onEachFeature: onEachCensusFeature
+        }).addTo(map_census);
+
+    // Get the bounds for the census tract geometries.
+    // Center and zoom the map.
+    $.ajax({
+        type: "GET",
+        url: census_url,
+        success: function (data) {
+
+            // remove the previous census tracts layer here
+            //
+            // do this by getting layers and removing them
+            map_census.eachLayer(function(layer){
+                //if(layer._tiles){
+                //    var a = 0;
+                ////} else if( d._popup ) {
+                ////    var a = 0;
+                //} else {
+                //    //console.log(d['_features']['properties']['name']);
+                //    //console.log(d);
+                //}
+
+                //d['_layers'].each(function(d2){
+                //    d2.remove();
+                //});
+                //layer.removeLayer(map_census);
+                if(layer._tiles) {
+                    var a = 0;
+                } else {
+                    // this also removes census_geoj, 
+                    // so we have to re-add it...
+                    map_census.removeLayer(layer);
+                }
+            });
+
+            census_geoj.addTo(map_census);
+            census_geoj.addData(data);
+            var bounds = census_geoj.getBounds();
+            //map_census.panInsideBounds(bounds);
+            //map_census.setZoom( map_census.getBoundsZoom(bounds) );
+            map_census.fitBounds(bounds,animate=true);
+
+
+        }
+    });
+
+    // Right here is the wrong place to use the 
+    // Ajax data to set the map bounds. 
+    //
+    // Ajax is asynchronous - so when we get to 
+    // this point in the script, the Ajax hasn't 
+    // been loaded yet. 
+    //
+    // It took me a long while to figure that out.
+
+
 }
 
 
+function onEachCensusFeature(f, l) {
+    if (f.properties) {
+        /*
+        l.on({
+            click: doClick,
+            mouseover: doMouseOver,
+            mouseout: doMouseOut
+        });
+        */
+        l.setStyle({  
+            fillColor: '#FCC',
+            fillOpacity: myFillOpacity,
+            stroke: true,
+            color: '#222',
+            weight: 1
+        });
+        l.bindPopup(f.properties['name']);
+    }
+}
