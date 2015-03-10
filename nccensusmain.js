63e669b4b1821a659d901eb4e0dc29b033977b9a
Charles Reid <charlesreid1@gmail.com>
2015-03-10 13:38:49 -0700

    updating nc census map, WILL THIS NEVER WORK???

--- nccensusmain.js
+++ nccensusmain.js
@@ -153,46 +153,36 @@ function doClick() {
     // so we need to use var these_layer_ids
     geoj.eachLayer(function(layer) {
 
-        those_layer_ids = Object.keys(layer._layers);
-        
-        if( layer['_options'] ){
+        that_layer_id = layer._leaflet_id;
+        var options = layer['options'];
 
-            those_layer_ids.forEach( function(that_layer_id) {
 
-                //console.log("this_layer_id = "+this_layer_id);
-                //console.log("that_layer_id = "+that_layer_id);
+        if( this_layer_id==that_layer_id ) {
 
-                // these_layer_ids usually has one element,
-                // unless a county has two non-continguous parts.
-                //
-                if( this_layer_id==that_layer_id ) {
+            var that_layer = layer['_layers'][that_layer_id]
+            var options = that_layer['options'];
 
-                    var that_layer = layer['_layers'][that_layer_id]
-                    var options = that_layer['options'];
+            if(options['fillColor']){
 
-                    if(options['fillColor']){
+                // Get the county's current color.
+                orig_fillColor = options['fillColor'];
 
-                        // Get the county's current color.
-                        orig_fillColor = options['fillColor'];
+                // Check if county is already red.
+                // If not, make it red.
+                if( orig_fillColor===red) {
+                    var a=0;
 
-                        // Check if county is already red.
-                        // If not, make it red.
-                        if( orig_fillColor===red) {
-                            var a=0;
+                } else {
+                    // set style to red 
+                    that_layer.setStyle({
+                        'fillColor' : red,
+                        'fillOpacity' : myThickFillOpacity,
+                        'originalFillColor' : orig_fillColor
+                    });
+                }
+            }
 
-                        } else {
-                            // set style to red 
-                            that_layer.setStyle({
-                                'fillColor' : red,
-                                'fillOpacity' : myThickFillOpacity,
-                                'originalFillColor' : orig_fillColor
-                            });
-                        }
-                    }
-
-                } 
-            });
-        }
+        } 
     });
 
 
